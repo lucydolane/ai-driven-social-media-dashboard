954cbb1d3150f3b243f606feeaf0355b1e69d012
lucy <lucydolanegan@gmail.com>
2022-03-13 12:24:42 +0000

    updated twitter keys and search topics/langs

--- source/ec2_twitter_reader/twitter_reader_config.js
+++ source/ec2_twitter_reader/twitter_reader_config.js
@@ -1,11 +1,11 @@
 var twitter_config = module.exports = {
     twitter: {
-        consumer_key: 'Your_consumer key',
-        consumer_secret: 'Your_Consumer_secret',
-        access_token: 'Your_access_token',
-        access_token_secret: 'Your_access_token_secret'
+        consumer_key: 'XX5R6vFUYyUWuQ0C2SBOVyFsr',
+        consumer_secret: 'G7cyyo8g7qwtK20FaZww0XrQrOwamANGg1nwrqxuBg097HbCfP',
+        access_token: '1438323402-wh6Uqu0I3gWVI9Bso4yY4HS4XUx09TPtILIov2j',
+        access_token_secret: 'SLXrfQOvJQ6hwpVYmWzmwnkZBTituIPMeDPSpO4vn5AXV'
       },
-    topics: ['IPL', 'cricket', 'sports', 'match', 'team'],
-    languages: ['en'],
-    kinesis_delivery: 'TwitterDataAnalyticsBlogPo-IngestionFirehoseStream-U5NYP1W88KR0'
-    }
+    topics: ['Amazon', 'Putin', 'Ukraine', 'Russia', 'War'],
+    languages: ['en', 'es', 'de', 'fr', 'ar', 'pt'],
+    kinesis_delivery: 'Social-Media-Analytics-Tool-Stack-IngestionFirehose-uALWXopuGvMP'
+}
