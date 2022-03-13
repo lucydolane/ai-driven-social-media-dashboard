46aa4c6a4d59c8b3c0a52d844dd84bd2559fc589
lucydolane <74865757+lucydolane@users.noreply.github.com>
2022-03-13 12:20:46 +0000

    Added twitter_reader_config.js to EC2 node script

---
+++ source/ec2_twitter_reader/twitter_reader_config.js
@@ -0,0 +1,11 @@
+var twitter_config = module.exports = {
+    twitter: {
+        consumer_key: 'Your_consumer key',
+        consumer_secret: 'Your_Consumer_secret',
+        access_token: 'Your_access_token',
+        access_token_secret: 'Your_access_token_secret'
+      },
+    topics: ['IPL', 'cricket', 'sports', 'match', 'team'],
+    languages: ['en'],
+    kinesis_delivery: 'TwitterDataAnalyticsBlogPo-IngestionFirehoseStream-U5NYP1W88KR0'
+    }
