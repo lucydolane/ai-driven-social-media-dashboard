ea0046baccae2f2cec293cbdc560063b1b78f6ca
lucy <lucydolanegan@gmail.com>
2022-03-13 16:03:57 +0000

    changes made to twitter_reader_config.js

--- source/ec2_twitter_reader/twitter_reader_config.js
+++ source/ec2_twitter_reader/twitter_reader_config.js
@@ -3,9 +3,9 @@ var keys = require('./keys.js');
 var twitter_config = module.exports = {
     twitter: {
         consumer_key: keys.consumer_key,
-        consumer_secret: 'G7cyyo8g7qwtK20FaZww0XrQrOwamANGg1nwrqxuBg097HbCfP',
-        access_token: '1438323402-wh6Uqu0I3gWVI9Bso4yY4HS4XUx09TPtILIov2j',
-        access_token_secret: 'SLXrfQOvJQ6hwpVYmWzmwnkZBTituIPMeDPSpO4vn5AXV'
+        consumer_secret: keys.consumer_secret,
+        access_token: keys.access_token,
+        access_token_secret: keys.access_token_secret
       },
     topics: ['Amazon', 'Putin', 'Ukraine', 'Russia', 'War'],
     languages: ['en', 'es', 'de', 'fr', 'ar', 'pt'],
