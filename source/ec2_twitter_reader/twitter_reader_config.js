aca4d86c8dd291335ee78e456511594170a158bf
lucy <lucydolanegan@gmail.com>
2022-03-13 14:52:11 +0000

    extracted twitter keys into keys.js and added file to gitignore

--- source/ec2_twitter_reader/twitter_reader_config.js
+++ source/ec2_twitter_reader/twitter_reader_config.js
@@ -1,6 +1,8 @@
+var keys = require('./keys.js');
+
 var twitter_config = module.exports = {
     twitter: {
-        consumer_key: 'XX5R6vFUYyUWuQ0C2SBOVyFsr',
+        consumer_key: keys.consumer_key,
         consumer_secret: 'G7cyyo8g7qwtK20FaZww0XrQrOwamANGg1nwrqxuBg097HbCfP',
         access_token: '1438323402-wh6Uqu0I3gWVI9Bso4yY4HS4XUx09TPtILIov2j',
         access_token_secret: 'SLXrfQOvJQ6hwpVYmWzmwnkZBTituIPMeDPSpO4vn5AXV'
